3a542009c0f040fd76769d879bcdc1fd327ee13e
ahdbilal <55514721+ahdbilal@users.noreply.github.com>
2020-03-28 16:34:05 -0700

    Update read_dist_config.py

--- .github/workflows/lib/read_dist_config.py
+++ .github/workflows/lib/read_dist_config.py
@@ -3,13 +3,13 @@
 def main():
     import yaml,os,sys
 
-    current_branch=(sys.stdin.readlines())[0].split("\n")[0]
+    current_branch=(sys.stdin.readlines())[0]
 
     with open("/Users/ahmedbilal/Desktop/GH-AC-Demo/dist_config.yml", 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
     #for i in range(0,len(cfg))
-    if current_branch==cfg[0]['branch']:
+    if cfg[0]['branch'] in current_branch:
         cfg[0]['branch']
         cfg[0]['build']
         group=cfg[0]['destinations'].split(",")
