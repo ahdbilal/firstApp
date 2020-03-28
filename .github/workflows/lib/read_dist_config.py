3f53a113aa8828b1bc87c09cd521436632c1feeb
ahdbilal <55514721+ahdbilal@users.noreply.github.com>
2020-03-28 16:05:57 -0700

    Update read_dist_config.py

--- .github/workflows/lib/read_dist_config.py
+++ .github/workflows/lib/read_dist_config.py
@@ -1,1 +1,22 @@
+def main():
+    import yaml,os,sys
 
+    current_branch=sys.argv[1]
+
+    with open("/Users/ahmedbilal/Desktop/GH-AC-Demo/dist_config.yml", 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    #for i in range(0,len(cfg))
+    if current_branch==cfg[0]['branch']:
+        cfg[0]['branch']
+        cfg[0]['build']
+        group=cfg[0]['destinations'].split(",")
+        cfg[0]['mandatory_update']
+        cfg[0]['notify_testers']
+    else:
+        group=""
+
+    return (group)
+  
+if __name__== "__main__":
+  main()
