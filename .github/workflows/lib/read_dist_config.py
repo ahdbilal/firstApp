ec906c7276d4310b7fd3130c5084cdaf6ab06391
ahdbilal <55514721+ahdbilal@users.noreply.github.com>
2020-03-28 18:49:31 -0700

    Update read_dist_config.py

--- .github/workflows/lib/read_dist_config.py
+++ .github/workflows/lib/read_dist_config.py
@@ -1,24 +1,36 @@
-
-
 def main():
     import yaml,os,sys
 
+    param=sys.argv[1]
+
     current_branch=(sys.stdin.readlines())[0]
 
-    with open(".distribute/config.yml", 'r') as ymlfile:
+    with open("/Users/ahmedbilal/Desktop/GH-AC-Demo/dist_config.yml", 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
-    #for i in range(0,len(cfg))
-    if cfg[0]['branch'] in current_branch:
-        cfg[0]['branch']
-        cfg[0]['build']
-        group=cfg[0]['destinations'].split(",")
-        cfg[0]['mandatory_update']
-        cfg[0]['notify_testers']
+    for i in range(0,len(cfg)):
+        if cfg[i]['branch'] in current_branch:
+            build=cfg[i]['build']
+            destinations=cfg[i]['destinations'].split(",")
+            mandatory_update=cfg[i]['mandatory_update']
+            notify_testers=cfg[i]['notify_testers']
+            break
+        else:
+            build=""
+            destinations=""
+            mandatory_update=""
+            notify_testers=""
+    
+    if param=="build":
+        return print(build)
+    elif param=="destinations":
+        return print(str(destinations)[1:-1])
+    elif param=="mandatory_update":
+        return print(mandatory_update)
+    elif param=="notify_testers":
+        return print(notify_testers)
     else:
-        group=""
-    os.system(" echo \"::set-output name=id::qwe\"" )
-    return print(group[0])
+        return print(-1)
   
 if __name__== "__main__":
   main()
